7d0aacd967c00a9474b1b837b206e191d4f50a3b
chrfoyer <chrfoyer@gmail.com>
2023-02-13 00:00:23 +0100

    Structure, but fails to open
    I have simplified it down to what I want to test as the first iteration, but it does not open correctly yet. I am close; I can taste it.

--- Buttler.Frontend/ButtlerApp1/App.js
+++ Buttler.Frontend/ButtlerApp1/App.js
@@ -1,21 +1,52 @@
-import { View } from "react-native";
-import MapView from "react-native-maps";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  PermissionsAndroid,
+  SafeAreaView,
+} from "react-native";
+// Function to get permission for location
+const App = () => {
+  // state to hold number of butts
+  const [number, onChangeNumber] = React.useState("");
 
-export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <MapView
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-      />
+    <View style={styles.container}>
+      <Text>Welcome!</Text>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
+      </SafeAreaView>
+      <Button title="Submit Count" />
     </View>
   );
-}
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+export default App;
