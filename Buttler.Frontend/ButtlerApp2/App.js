8cc4839747b6f768fd8956ade08433608d294945
chrfoyer <chrfoyer@gmail.com>
2023-02-13 17:54:33 +0100

    feat: Geolocation start
    You can now locate yourself using the app. The coordinates then show in the get location button. This is accomplished using the expo-location library, which actually works instead of the geolocation service library. Development is still needed for iOs

--- Buttler.Frontend/ButtlerApp2/App.js
+++ Buttler.Frontend/ButtlerApp2/App.js
@@ -1,11 +1,65 @@
-import React from "react";
-import { Button, View, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  Button,
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  PermissionsAndroid,
+} from "react-native";
+import * as Location from "expo-location";
+
+// Function to get permission for location
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Geolocation Permission",
+        message: "Can we access your location?",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK",
+      }
+    );
+    console.log("granted", granted);
+    if (granted === "granted") {
+      console.log("You can use Geolocation");
+      return true;
+    } else {
+      console.log("You cannot use Geolocation");
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+};
 
 const ButtCounter = () => {
-  const [number, onChangeNumber] = React.useState("Beer");
+  const [number, onChangeNumber] = React.useState("");
+  const [location, setLocation] = useState(false);
+  const [viewLocation, isViewLocation] = useState([]);
+
+  // function to check permissions and get Location
+  const getLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Permission denied. Enter the location manually.");
+      return;
+    }
+        const location = await Location.getCurrentPositionAsync({});
+        console.log(location);
+        setLocation(location);
+    };
 
   return (
-    <View style={{ justifyContent: "center", height: "100%" }}>
+    <View
+      style={{
+        alignContent: "center",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
       <TextInput
         style={styles.input}
         onChangeText={onChangeNumber}
@@ -29,6 +83,13 @@ const ButtCounter = () => {
           })
         }
       />
+      <View
+        style={{ marginTop: 10, padding: 10, borderRadius: 10, width: "40%" }}
+      >
+        <Button title="Get Location" onPress={getLocation} />
+      </View>
+      <Text>Latitude: {location ? location.coords.latitude : null}</Text>
+      <Text>Longitude: {location ? location.coords.longitude : null}</Text>
     </View>
   );
 };
@@ -39,6 +100,12 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 20,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 export default ButtCounter;
