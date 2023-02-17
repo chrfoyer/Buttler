bee948b49ef7c7801dd5cf6ea7c985d342d135a6
MartyBobo <martin.rosendahl4@gmail.com>
2023-02-17 13:12:25 +0100

    update db and latlon
    created lat and lon for reports and updated the database to hold those as well.

---
+++ Buttler.Backend/Buttler.Data/Migrations/20230217121106_latlon.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Buttler.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class latlon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "latitude",
+                table: "Reports",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "longitude",
+                table: "Reports",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "latitude",
+                table: "Reports");
+
+            migrationBuilder.DropColumn(
+                name: "longitude",
+                table: "Reports");
+        }
+    }
+}
