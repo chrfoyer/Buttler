1396df6bb31505ce45162ec3a2a931855b7afa81
chrfoyer <chrfoyer@gmail.com>
2023-03-26 23:52:51 +0200

    feat: See your current location
    There is now a button to see the current user location as well as a familiar blue circle on the map to represent it.

--- Buttler.Frontend/ButtlerApp2/App.js
+++ Buttler.Frontend/ButtlerApp2/App.js
@@ -14,7 +14,7 @@ import * as Location from "expo-location";
 
 const ButtCounter = () => {
   const [number, onChangeNumber] = React.useState("");
-  const [location, setLocation] = useState(false);
+  const [location, setLocation] = useState(null);
   const [markers, setMarkers] = useState([]);
   const [loading, setLoading] = useState(true);
   const appState = useRef(AppState.currentState);
@@ -22,14 +22,19 @@ const ButtCounter = () => {
 
   // function to check permissions and get Location
   const getLocation = async () => {
-    const { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== "granted") {
-      console.log("Permission denied. Enter the location manually.");
-      return;
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission denied. Enter the location manually.");
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setLocation(location);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error", "Failed to get location. Please try again.");
     }
-    const location = await Location.getCurrentPositionAsync({});
-    console.log(location);
-    setLocation(location);
   };
 
   useEffect(() => {
@@ -54,23 +59,31 @@ const ButtCounter = () => {
     };
   }, []);
 
-  const sendCount = () => {
-    fetch("http://34.90.196.163/api/Reports", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        userName: "anon",
-        numberOfWaste: number,
-        wasteType: 1,
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      }),
-    });
-    console.log(number + " butts logged");
-    Alert.alert("Success", "You logged " + number + " butts");
+  const sendCount = async () => {
+    try {
+      const response = await fetch("http://34.90.196.163/api/Reports", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName: "anon",
+          numberOfWaste: number,
+          wasteType: 1,
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to submit count");
+      }
+      console.log(number + " butts logged");
+      Alert.alert("Success", "You logged " + number + " butts");
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error", "Failed to submit count. Please try again.");
+    }
   };
 
   const getMarkers = async () => {
@@ -96,35 +109,35 @@ const ButtCounter = () => {
       <View style={styles.button}>
         <Button title="Submit" onPress={sendCount} style />
       </View>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: 55.86219474240435,
-          longitude: 9.84887225819323,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-        showUserLocation={true}
-      >
-        <Marker
-          coordinate={{
-            latitude: 55.86217798371234,
-            longitude: 9.851928848679208,
+      {location && (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
           }}
-        />
-        {markers.map((marker, index) => (
-          <Marker
-            key={index}
-            coordinate={{
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+        >
+          <Heatmap
+            points={markers.map((marker) => ({
               latitude: marker.latitude,
               longitude: marker.longitude,
+              weight: marker.numberOfWaste,
+            }))}
+            opacity={1}
+            radius={50}
+            gradient={{
+              colors: ["#00ADEF", "#00639C", "#FFC500", "#FF6900", "#FF0D00"],
+              startPoints: [0.01, 0.25, 0.5, 0.75, 1],
+              colorMapSize: 256,
             }}
-            title={marker.title}
-            description={marker.description}
           />
-        ))}
-      </MapView>
+        </MapView>
+      )}
     </View>
   );
 };
